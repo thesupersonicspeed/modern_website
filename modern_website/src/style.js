012346ac3aad610b256e4278b74651f304869069
thesupersonicspeed <116554164+thesupersonicspeed@users.noreply.github.com>
2023-09-08 15:57:06 +0100

    v1.0

--- modern_website/src/style.js
+++ modern_website/src/style.js
@@ -10,19 +10,19 @@ const styles = {
     paddingX: "sm:px-16 px-6",
     paddingY: "sm:py-16 py-6",
     padding: "sm:px-16 px-6 sm:py-12 py-4",
-
+  
     marginX: "sm:mx-16 mx-6",
     marginY: "sm:my-16 my-6",
-};
-
-export const layout = {
+  };
+  
+  export const layout = {
     section: `flex md:flex-row flex-col ${styles.paddingY}`,
     sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
-
+  
     sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
     sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
-
+  
     sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
-};
-
-export default styles;
+  };
+  
+  export default styles;``
