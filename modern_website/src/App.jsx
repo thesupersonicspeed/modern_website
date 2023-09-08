efbb8106c3f6d86ffdb409614b272c505740843a
thesupersonicspeed <116554164+thesupersonicspeed@users.noreply.github.com>
2023-09-08 16:56:28 +0100

    v1.2

--- modern_website/src/App.jsx
+++ modern_website/src/App.jsx
@@ -4,7 +4,7 @@ import styles from './style';
 import Billing from './components/Billing';
 import Business from './components/Business';
 import  Button from './components/Button';
-import  CardDealfrom './components/CardDeal';
+import  CardDeal from './components/CardDeal';
 import Clients from './components/Clients';
 import CTA from './components/CTA';
 import FeedbackCard from './components/FeedbackCard';
@@ -12,8 +12,8 @@ import  Footer from './components/Footer';
 import GetStarted from './components/';
 import  Hero from './components/Hero';
 import Navbar  from './components/Navbar';
-import  from './components/';
-
+import  Stats from './components/Stats';
+import  Testimonials from './components/Testimonials';
 const App = () =>   (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
