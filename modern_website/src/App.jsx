b6644d73b2a0658b19407216dab5eb38976870a7
alae29 <101356809+alae29@users.noreply.github.com>
2023-09-08 16:24:12 +0200

    v

--- modern_website/src/App.jsx
+++ modern_website/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import styles from './style';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () =>   (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          navbar
+        </div>
+       </div>
+
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            Hero
+          </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            Hero
+          </div>
+      </div>
+
+    </div>
+  );
 
 export default App
